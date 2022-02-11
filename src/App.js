0d53e484ed291fe447fb3658d449b53a3c7d8dc9
Cecilia Garcia <ceciliagarcia@MacBook-Pro-de-Cecilia.local>
2022-02-11 18:47:19 -0300

    hopeless

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react"
 import Sidebar from "./components/Sidebar"
 import NavbarTop from "./components/Navbar"
 
+import './App.css';
 import Container from 'react-bootstrap/Container';
 
 class App extends Component {
@@ -21,4 +22,4 @@ class App extends Component {
   }
 }
 
-export default App 
+export default App; 
